2b0b53406a59502dd453412fb9421164435aa6bf
Mark Arts <piderman+github@gmail.com>
2018-05-02 11:27:09 +0200

    Make executable

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,35 @@
+extern crate brainrot;
+
+use std::env;
+use std::fs::File;
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        println!("Please provide one argument, that is the bf source file.");
+        return;
+    }
+
+    let filename = &args[1];
+
+    let mut file = match File::open(filename) {
+        Ok(f) => f,
+        Err(_) => {
+            println!("Could not open file {}", filename);
+            return;
+        }
+    };
+
+    let mut code = Vec::new();
+    match file.read_to_end(&mut code) {
+        Ok(_) => (),
+        Err(_) => {
+            println!("Failed to read file {}", filename);
+            return;
+        }
+    }
+
+    brainrot::run(code, &mut std::io::stdin(), &mut std::io::stdout());
 }
